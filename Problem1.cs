b6e2b578783a09585a03b253ef96184a6719d231
Glen Mc Lelland <glen.mc-lelland@web.de>
2022-08-01 00:11:46 +0200

    Solved Testproblem 1 on CodeAbbey Testing the Waters in VS Code

---
+++ Problem1.cs
@@ -0,0 +1,12 @@
+namespace Problem1 {
+
+    class Solution {
+        
+        public string solve(int p1, int p2) {
+            var a = p1 + p2;
+            return a.ToString();
+        }
+
+    }
+
+}
