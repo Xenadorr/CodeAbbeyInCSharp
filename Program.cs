2bee2357c2ba11d437f7e590ae24db17a562db4b
Glen Mc Lelland <glen.mc-lelland@web.de>
2022-08-01 00:10:50 +0200

    Solved Testproblem 1 on CodeAbbey Testing the Waters in VS Code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Problem1;
+class Start
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 1st Number:");
+        var s1 = Console.ReadLine();
+        int n1 = Int32.Parse(s1);
+
+        Console.WriteLine("Enter 2nd Number:");
+        var s2 = Console.ReadLine();
+        int n2 = Int32.Parse(s2);
+
+        var p = new Solution();
+        
+        Console.WriteLine("Answer is: {0}", p.solve(n1, n2));
+    }
+}
