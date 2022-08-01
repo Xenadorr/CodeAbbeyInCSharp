e3f4388ab29957b0333a6949597fa012cdb5fd82
Glen Mc Lelland <glen.mc-lelland@web.de>
2022-08-01 11:28:12 +0200

    Updated the structure of problem solving New Namespace for each Problem

---
+++ Problems/Problem1.cs
@@ -0,0 +1,10 @@
+namespace Problem.One {
+
+    class Solution {       
+        public string solve() {
+            string[] tokens = Console.ReadLine().Split(' ');
+            var a = int.Parse(tokens[0]) + int.Parse(tokens[1]);
+            return a.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-﻿using Problem1;
+﻿
 class Start
 {
      static void Main(string[] args)
     {
-        Console.WriteLine("Enter 1st Number:");
-        var s1 = Console.ReadLine();
-        int n1 = Int32.Parse(s1);
+        int probleNr = 1;
 
-        Console.WriteLine("Enter 2nd Number:");
-        var s2 = Console.ReadLine();
-        int n2 = Int32.Parse(s2);
+        switch (probleNr)
+        {
+            case 1:
+                var p = new Problem.One.Solution();
+                Console.WriteLine("Answer is: {0}", p.solve());
+                break;
+            default:
 
-        var p = new Solution();
-        
-        Console.WriteLine("Answer is: {0}", p.solve(n1, n2));
+                break;
+        }
+
     }
 }
