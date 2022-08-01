129b0887e76d2c0710aec8e957d2bd2f508c2120
Glen Mc Lelland <glen.mc-lelland@web.de>
2022-08-01 13:48:17 +0200

    Nullcheck

--- Problems/Problem2.cs
+++ Problems/Problem2.cs
@@ -2,12 +2,14 @@ namespace Problem.Two {
 
     class Solution {       
         public string solve() {
-            string amount = Console.ReadLine();
-            string[] tokens = Console.ReadLine().Split();
+            string? amount = Console.ReadLine();
+            string[] tokens = Console.ReadLine()
+                .Split();
             int sum = 0;
             foreach (var item in tokens)
             {
-                sum += Int32.Parse(item);
+                if (!String.IsNullOrEmpty(item))
+                    sum += Int32.Parse(item);
             }
             return sum.ToString();
         }
