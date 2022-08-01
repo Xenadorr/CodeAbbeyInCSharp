c44f2b68c0af3bd85891bdb15478b9b052063b0b
Glen Mc Lelland <glen.mc-lelland@web.de>
2022-08-01 13:56:45 +0200

    NullForgiveness

--- Problems/Problem1.cs
+++ Problems/Problem1.cs
@@ -2,7 +2,7 @@ namespace Problem.One {
 
     class Solution {       
         public string solve() {
-            string[] tokens = Console.ReadLine()
+            string[] tokens = Console.ReadLine()!
                 .Split(' ');
             var a = int.Parse(tokens[0]) + int.Parse(tokens[1]);
             return a.ToString();
--- Problems/Problem2.cs
+++ Problems/Problem2.cs
@@ -3,7 +3,7 @@ namespace Problem.Two {
     class Solution {       
         public string solve() {
             string? amount = Console.ReadLine();
-            string[] tokens = Console.ReadLine()
+            string[] tokens = Console.ReadLine()!
                 .Split();
             int sum = 0;
             foreach (var item in tokens)
