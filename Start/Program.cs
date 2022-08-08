bd17e08b6bb330b8a349a7d1b3068cb491d973a3
Glen Mc Lelland <glen.mc-lelland@web.de>
2022-08-08 20:55:21 +0200

    Only 1 CW

--- Start/Program.cs
+++ Start/Program.cs
@@ -3,30 +3,34 @@ class Start
 {
      static void Main(string[] args)
     {
-        int problemNr = 4;
+        int problemNr = Int32.Parse(Console.ReadLine()!);
+        string answer = "Nothing";
 
         switch (problemNr)
         {
             case 1:
                 var problem1 = new Problem1();
-                Console.WriteLine("Answer to problem Nr {0}: {1}", problemNr.ToString(), problem1.solve());
+                answer = problem1.solve();
                 break;
             case 2:
                 var problem2 = new Problem2();
-                Console.WriteLine("Answer to problem Nr {0}: {1}", problemNr.ToString(), problem2.solve());
+                answer = problem2.solve();
                 break;
             case 3:
                 var problem3 = new Problem3();
-                Console.WriteLine("Answer to problem Nr {0}: {1}", problemNr.ToString(), problem3.solve());
+                answer = problem3.solve();
                 break;
             case 4:
                 var problem4 = new Problem4();
-                Console.WriteLine("Answer to problem Nr {0}: {1}", problemNr.ToString(), problem4.solve());
+                answer = problem4.solve();
                 break;
             default:
                 System.Console.WriteLine("Please chose a valid problem.");
                 break;
         }
 
+
+        System.Console.WriteLine("Answer to problem Nr {0}: {1}", problemNr, answer);
     }
+
 }
